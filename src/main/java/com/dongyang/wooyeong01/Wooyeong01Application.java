fe17ea7003ef4c01cb9a4a9c37d73a8d5159c5e1
JUY021 <wjsdndud02@gmail.com>
2025-03-31 20:19:38 +0900

    250331 2202 포트 번호 8090으로 수정

---
+++ src/main/java/com/dongyang/wooyeong01/Wooyeong01Application.java
@@ -0,0 +1,13 @@
+package com.dongyang.wooyeong01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wooyeong01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Wooyeong01Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/dongyang/wooyeong01/controller/FirstController.java
@@ -0,0 +1,16 @@
+package com.dongyang.wooyeong01.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+    @GetMapping("/hi")
+    public String niceToMeet(){
+        return "greetings";
+    }
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+}
