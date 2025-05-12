488e75177ba34c2eee5a77d602d80a4dea95d7e3
JUY021 <wjsdndud02@gmail.com>
2025-05-12 11:50:37 +0900

    250512 1150 REST API 추가함

--- src/main/java/com/dongyang/wooyeong01/dto/ArticleForm.java
+++ src/main/java/com/dongyang/wooyeong01/dto/ArticleForm.java
@@ -1,41 +1,20 @@
 package com.dongyang.wooyeong01.dto;
 
 import com.dongyang.wooyeong01.entity.Article;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+@ToString
+@Getter
+@Setter
 public class ArticleForm {
     private Long id;
     private String title;
     private String content;
 
-    public Long getId() { return id; }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setId(Long id) { this.id = id; }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
     public Article toEntity() {
         return new Article(id, title, content);
     }
 
-    @Override
-    public String toString() {
-        return "ArticleForm{" +
-                "title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 }
